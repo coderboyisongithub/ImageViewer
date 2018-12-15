709a807811dc7a9675db8b6b5c6417f1c709f2b1
Spider~Byte <rajshivanshu19@gmail.com>
2018-12-15 22:35:16 +0545

    Binaries
    Contains executables for x64 machine..

---
+++ bin x64/shader.glsl
@@ -0,0 +1,43 @@
+    #shader vertex
+    #version 330 core 
+
+    layout(location = 0) in vec4 positions;
+  layout(location=0) in vec2 texCoords;
+
+    uniform mat4 translation_matrix; //inactive shader variables..
+    uniform mat4 rotation_matrix;
+
+    mat4 model_matrix; //
+    mat4 projection_matrix;
+    out vec4 pos;
+    out vec2 frag_TexCoord;
+
+    void main()
+    {
+    
+    rotation_matrix;
+
+    gl_Position=translation_matrix*positions; //translation matrix operated with POS vector..
+    pos=gl_Position;
+frag_TexCoord=texCoords;
+    translation_matrix;
+
+    }
+
+
+    #shader fragment  
+    #version 330 core
+
+    layout(location=0) out vec4 color;
+    in vec4 pos;
+    in vec2 frag_TexCoord;
+    uniform float col;
+uniform sampler2D a_texture;
+
+
+    void main()
+    {
+    col;
+    color=texture(a_texture,frag_TexCoord);
+
+    }
